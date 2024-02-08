343fd26a7600c6314d3dc20846437b0b7a5c85b9
JohnGastone <johngastone11@gmail.com>
2024-02-08 13:50:09 +0300

    searchPage()

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:tafuta/searchPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(home: SearchPage());
   }
 }
