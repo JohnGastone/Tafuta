9b7b9208c39fa9e20d36932e47bca6ad40d1286b
JohnGastone <johngastone11@gmail.com>
2024-02-09 18:14:11 +0300

    movieModel

--- lib/model/movie_model.dart
+++ lib/model/movie_model.dart
@@ -1,1 +1,6 @@
-import 'package:flutter/material.dart';
+class MovieModel {
+  String? movieTitle;
+  int? releaseYear;
+  String? posterURL;
+  MovieModel(this.movieTitle, this.releaseYear, this.posterURL);
+}
