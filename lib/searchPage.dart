27046f1742a6f382dc8ea21a66cdfdc3606baf50
JohnGastone <johngastone11@gmail.com>
2024-02-09 23:23:55 +0300

    Dummy movies list

--- lib/searchPage.dart
+++ lib/searchPage.dart
@@ -2,6 +2,7 @@
 // ignore: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:tafuta/model/movie_model.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -11,6 +12,20 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  static List<MovieModel> movieList = [
+    MovieModel('Panguso', 2020, 'images/Tafuta.png'),
+    MovieModel('Chausiku', 2013, 'images/Tafuta.png'),
+    MovieModel('Gusa Unase', 2021, 'images/Tafuta.png'),
+    MovieModel('Mwana Ukome', 217, 'images/Tafuta.png'),
+    MovieModel('Mama Kimbo', 2020, 'images/Tafuta.png'),
+    MovieModel('Kuku Mgeni', 2023, 'images/Tafuta.png'),
+    MovieModel('Paka Shume', 2020, 'images/Tafuta.png'),
+    MovieModel('Ukiinama Ntiti ukiinuka Ntiti', 2021, 'images/Tafuta.png'),
+    MovieModel('Mama Kilakshal', 2020, 'images/Tafuta.png'),
+    MovieModel('Mama Mbelemende', 2013, 'images/Tafuta.png'),
+    MovieModel('Kijiwe Samli', 2024, 'images/Tafuta.png'),
+  ];
+
   void updateList(String value) {
     // We will filter our list of movies here
   }
@@ -22,8 +37,9 @@ class _SearchPageState extends State<SearchPage> {
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
         elevation: 0.4,
+        centerTitle: true,
         title: Text(
-          "Movie Arena",
+          "Movies Arena",
           style: TextStyle(fontSize: 22),
         ),
       ),
