a89b0473d00adf470514fbbdf6a1ca8348804d23
JohnGastone <johngastone11@gmail.com>
2024-02-14 15:05:24 +0300

    Changing brightness

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SearchPage());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            // visualDensity: VisualDensity.AdaptivePlatformDensity,
+            brightness: Brightness.dark),
+        home: SearchPage());
   }
 }
