cb0ab6e95506dc1c12aa92e80c98ff70320c42c7
JohnGastone <johngastone11@gmail.com>
2024-02-19 09:41:01 +0300

    Updating movie_model data

--- lib/movieDescription.dart
+++ lib/movieDescription.dart
@@ -178,37 +178,20 @@ class ItemPage extends StatelessWidget {
                       children: [
                         Text(
                           'Number of Downloads:   ',
-                          style: TextStyle(
+                          style: GoogleFonts.spaceMono(
                               fontSize: 18,
                               fontWeight: FontWeight.w400,
                               color: Colors.blueGrey),
                         ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Row(
-                          children: [
-                            for (int i = 1; i < 6; i++)
-                              Container(
-                                height: 20,
-                                width: 20,
-                                alignment: Alignment.center,
-                                margin: EdgeInsets.symmetric(horizontal: 5),
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(30),
-                                    boxShadow: [
-                                      BoxShadow(
-                                          color: Colors.grey.withOpacity(0.5),
-                                          spreadRadius: 3,
-                                          blurRadius: 8)
-                                    ]),
-                                child: Text(i.toString(),
-                                    style: TextStyle(
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.w300)),
-                              )
-                          ],
+                        // SizedBox(
+                        //   width: 10,
+                        // ),
+                        Text(
+                          '21873',
+                          style: GoogleFonts.spaceMono(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.purpleAccent),
                         )
                       ],
                     ),
