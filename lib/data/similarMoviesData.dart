e5fda63156431d86389a56a8a24e3f197f4b590a
JohnGastone <johngastone11@gmail.com>
2024-04-14 08:19:11 +0300

    similar movies genres

--- lib/data/similarMoviesData.dart
+++ lib/data/similarMoviesData.dart
@@ -4,30 +4,33 @@ import '../model/similarMovies_model.dart';
 
 final List<SimilarMovies> similarMovies = [
   SimilarMovies(
-    movieTitle: "Safari ya Gwalu",
-    movieRating: 4.6,
-    movieImage: 'images/gwalu.jpg',
-    releaseYear: 2014,
-  ),
+      movieTitle: "Safari ya Gwalu",
+      movieRating: 4.6,
+      movieImage: 'images/gwalu.jpg',
+      releaseYear: 2014,
+      genre: 'Adventure'),
   SimilarMovies(
-    movieTitle: "Chausiku",
-    movieRating: 4.2,
-    movieImage: 'images/chausiku.jpeg',
-    releaseYear: 2013,
-  ),
+      movieTitle: "Chausiku",
+      movieRating: 4.2,
+      movieImage: 'images/chausiku.jpeg',
+      releaseYear: 2013,
+      genre: 'Swahili Life'),
   SimilarMovies(
       movieTitle: "Mama Mbelemende",
       movieRating: 5.0,
       movieImage: 'images/kilakshal.jpeg',
-      releaseYear: 2014),
+      releaseYear: 2014,
+      genre: 'Comedy'),
   SimilarMovies(
       movieTitle: "More than a lion",
       movieRating: 4.2,
       movieImage: 'images/gusa.jpeg',
-      releaseYear: 2012),
+      releaseYear: 2012,
+      genre: 'Comedy'),
   SimilarMovies(
       movieTitle: "Juha",
       movieRating: 4.5,
       movieImage: 'images/mwana.jpeg',
-      releaseYear: 2008)
+      releaseYear: 2008,
+      genre: 'Comedy'),
 ];
